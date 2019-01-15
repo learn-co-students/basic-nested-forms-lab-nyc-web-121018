f6cbc6d9fe1e6abc142908dd5032ce6e5a42d4ae
tfuquirk <timothyfquirk@gmail.com>
2019-01-15 17:53:49 -0500

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
 end
+
+
+# recipe.ingredients_attributes={"0"=>{"name"=>"keylimes", "quantity"=>"2/3 cups lime juice"}, 
+# "1" => {"name"=>"eggs", "quantity"=>"2 large eggs"}}
---
+++ db/migrate/20190115225059_add_columningredient_type_ingredients.rb
@@ -0,0 +1,5 @@
+class AddColumningredientTypeIngredients < ActiveRecord::Migration
+  def change
+    add_column :ingredients, :ingredient_type, :string
+  end
+end
